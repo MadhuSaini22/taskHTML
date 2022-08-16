c9d8cd72a60d1b1e0582cb4e9d74acf0b3fdd58a
MadhuSaini22 <madhusaini46810@gmail.com>
2022-08-16 17:58:52 +0530

    initial commit

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function filterForm() {
 
     let title = cards[i].querySelector(".title");
     let titleText = title.innerText || title.textContent;
-
+    document.getElementById("btn").style.visibility = "visible";
     if (
       text.indexOf(exactValue) > -1 ||
       titleText.indexOf(exactValue) > -1 ||
@@ -83,12 +83,14 @@ function Reset() {
   let sortedData = undefined;
   data = reCall(data, sortedData, notToChange);
   document.getElementById("cards").innerHTML = data;
+  document.getElementById("btn").style.visibility = "hidden";
 }
 
 function filterByCategory() {
   var e = document.getElementById("filterdown");
   var text = e.options[e.selectedIndex].text.toLowerCase();
   console.log(text);
+  document.getElementById("btn").style.visibility = "visible";
   let container = document.getElementById("cards");
   let cards = container.getElementsByClassName("card");
   for (var i = 0; i < cards.length; i++) {
@@ -103,7 +105,7 @@ function filterByCategory() {
 
 function Sorting() {
   sortedData = [...notToChange];
-
+  document.getElementById("btn").style.visibility = "visible";
   var e = document.getElementById("sortdown");
   var value = e.value;
   var selected = value.toLowerCase();
@@ -144,19 +146,28 @@ function pageCall(e) {
   for (var i = start - 1; i < e * 4; i++) {
     cards[i].style.display = "";
   }
-}
-$(document).ready(function () {
-  let name = localStorage.getItem("UserName");
-  logoutHelper(name);
-});
-
-function logoutHelper(name) {
-  if (name == null) {
-    document.getElementById("userIn").innerHTML = "Welcome " + "ghost";
-  } else {
-    document.getElementById("userIn").innerHTML = "Welcome " + name;
+  let listcontainer = document.getElementById("page");
+  let list = listcontainer.getElementsByClassName("page-item");
+  console.log(list);
+  for (var i = 0; i < list.length; i++) {
+    list[i].style.color = "";
+    list[i].classList.remove("active");
   }
+  list[e - 1].style.color = "blue";
+  list[e - 1].classList.add("active");
 }
+// $(document).ready(function () {
+//   let name = localStorage.getItem("UserName");
+//   logoutHelper(name);
+// });
+
+// function logoutHelper(name) {
+//   if (name == null) {
+//     document.getElementById("userIn").innerHTML = "Welcome " + "ghost";
+//   } else {
+//     document.getElementById("userIn").innerHTML = "Welcome " + name;
+//   }
+// }
 
 function logout() {
   localStorage.removeItem("UserName");
@@ -179,4 +190,34 @@ window.onclick = function (event) {
   }
 };
 
+let Username = localStorage.getItem("UserName");
 
+if (Username == null) {
+  let ui = `<a href="signup.html">Signup</a>
+    <a href="login1.html">Sign In</a>`;
+  document.getElementById("myDropdown").innerHTML = ui;
+  console.log({ document: document.querySelectorAll(".dropdown1") });
+} else if (Username) {
+  let ui = `<a href="#" onclick="logout()">Log Out</a>`;
+  document.getElementById("myDropdown").innerHTML = ui;
+  console.log({ document: document.querySelectorAll(".dropdown1") });
+  let dropdown = document.getElementById("myDropdown");
+  dropdown.style.bottom = "90%";
+}
+
+// document.getElementById("page")
+// .addEventListener("click", function() {
+//   if (this.classList.contains("active")) {
+//     this.classList.remove("active");
+//   } else this.classList.add("active");
+// });
+// function changeClass(e) {
+//   console.log(e)
+//   let container = document.getElementById("page");
+//   let list = container.getElementsByClassName("current-page");
+//   console.log(list);
+//   for (var i = 0; i < list.length; i++) {
+//     list[i].style.color = "white";
+//   }
+//   list[e].style.color=""
+// }
